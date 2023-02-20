84723063e484cfc858c19468ea832d6045df8bd7
David Cornett <davidgcornett@gmail.com>
2023-02-20 14:07:40 -0500

    func returns false if all numbers

--- check_pwd.py
+++ check_pwd.py
@@ -1,7 +1,7 @@
 
 def check_pwd(password):
     if 7 < len(password) < 21:
-        return contains_upper_case(password) and contains_lower_case(password)
+        return contains_upper_case(password) and contains_lower_case(password) and contains_letter(password)
     else:
         return False
 
@@ -10,3 +10,10 @@ def contains_upper_case(text):
 
 def contains_lower_case(text):
     return False if text.isupper() else True
+
+def contains_letter(text):
+    for char in text:
+        if char.isalpha():
+            return True
+    return False
+    
