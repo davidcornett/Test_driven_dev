c55c37645e094125f61dc6e469b9625ac586fabc
David Cornett <davidgcornett@gmail.com>
2023-02-20 17:09:30 -0500

    func returns true if at least 1 num and 1 letter

--- check_pwd.py
+++ check_pwd.py
@@ -1,7 +1,7 @@
 
 def check_pwd(password):
     if 7 < len(password) < 21:
-        return contains_upper_case(password) and contains_lower_case(password) and contains_letter(password)
+        return contains_upper_case(password) and contains_lower_case(password) and contains_correct_chars(password)
     else:
         return False
 
@@ -11,9 +11,13 @@ def contains_upper_case(text):
 def contains_lower_case(text):
     return False if text.isupper() else True
 
-def contains_letter(text):
+def contains_correct_chars(text):
+    num = False
+    letter = False
     for char in text:
         if char.isalpha():
-            return True
-    return False
+            letter = True
+        elif char.isdigit():
+            num = True
+    return num and letter
     
