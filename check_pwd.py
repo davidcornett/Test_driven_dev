9d146e8890adc0acdcb149722bba448774fe6cc0
David Cornett <davidgcornett@gmail.com>
2023-02-20 13:09:59 -0500

    func returns true only if pwd contains at least 1 uppercase, else false. Also fixed < bug in length checler

--- check_pwd.py
+++ check_pwd.py
@@ -1,6 +1,9 @@
 
 def check_pwd(password):
-    if 7 > len(password) < 21:
-        return True
+    if 7 < len(password) < 21:
+        return check_lowercase(password)
     else:
         return False
+
+def check_lowercase(text):
+    return False if text.islower() else True
--- tests.py
+++ tests.py
@@ -7,19 +7,16 @@ class TestCheckPWDFunc(unittest.TestCase):
     # test short borderline pwd
     def test1(self):
         pwd = "abc1234"
-        check_pwd(pwd)
         self.assertFalse(check_pwd(pwd), msg='Check_PWD_Test({})'.format(pwd))
 
     # test long borderline pwd
     def test2(self):
         pwd = "abc123456712345671234"
-        check_pwd(pwd)
         self.assertFalse(check_pwd(pwd), msg='Check_PWD_Test({})'.format(pwd))
 
     # test no lowercase letters but correct length
     def test3(self):
         pwd = "abcdefghi"
-        check_pwd(pwd)
         self.assertFalse(check_pwd(pwd), msg='Check_PWD_Test({})'.format(pwd))
 
 if __name__ == '__main__':
